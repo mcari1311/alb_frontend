9d3af1b3b881bed83e9e974bc1c64d039b72da26
Marie <mcari1311@gmail.com>
2022-09-07 17:40:17 -0400

    changed links

--- learn-alb/src/services/alb.api.js
+++ learn-alb/src/services/alb.api.js
@@ -10,26 +10,26 @@ export const getAlb = () => {
 }
 
 export const getAlbSing = (id) => {
-    const URL = `http://localhost:3001/alb/${id}`
+    const URL = `https://learnalb-backend.herokuapp.com/alb/${id}`
     const response = axios.get(URL)
     console.log(response)
     return response 
 }
 
 export const deleteAlbSing = (id) => {
-    const URL = `http://localhost:3001/alb/${id}`
+    const URL = `https://learnalb-backend.herokuapp.com/alb/${id}`
     const response = axios.delete(URL)
     return response
 }
 
 export const editAlbSing = (id, updatedAlbSing) => {
-    const URL = `http://localhost:3001/alb/${id}`
+    const URL = `https://learnalb-backend.herokuapp.com/alb/${id}`
     const response = axios.put(URL, updatedAlbSing)
     return response
 }
 
 export const createAlbSing = (albSing) => {
-    const URL = 'http://localhost:3001/alb'
+    const URL = 'https://learnalb-backend.herokuapp.com/alb'
     const response = axios.post(URL, albSing)
     return response 
 }
